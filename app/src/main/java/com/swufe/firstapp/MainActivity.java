4efc8b3d3646050170619a6676fd76d7df184679
Wyh987654321 <57186610+Wyh987654321@users.noreply.github.com>
2020-03-19 19:41:12 +0800

    生日贺卡

--- app/src/main/java/com/swufe/firstapp/MainActivity.java
+++ app/src/main/java/com/swufe/firstapp/MainActivity.java
@@ -2,23 +2,58 @@ package com.swufe.firstapp;
 
 import android.os.Bundle;
 
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
-
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
 
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class
-MainActivity extends AppCompatActivity {
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
-    @Override
+public class MainActivity extends AppCompatActivity {
+     EditText inp;
+     TextView out;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        inp=(EditText)findViewById(R.id.inp);
+        out=findViewById(R.id.out);
+    }
+    public void clik(View x){
+        String oldtem = inp.getText().toString();
+
+        if(oldtem!=null){
+                out.setText(gettem(oldtem));
+        }
+    }
+    //获取温度
+    public String gettem(String str){
+        boolean  isC=(str.contains("c")||str.contains("C"));
+        boolean  isF=(str.contains("f")||str.contains("F"));
+        int result;
+        String res;
+        int Truetem;
+        String regEx="[^0-9]";
+        Pattern p = Pattern.compile(regEx);
+        Matcher m = p.matcher(str);
+        String tem =m.replaceAll("").trim();
+        Truetem=Integer.parseInt(tem);
+        if(isC){
+            result=Truetem*9/5+32;
+            res =Integer.toString(result)+"F";
+        }
+        else if(isF){
+            result =(Truetem-32)*5/9;
+            res =Integer.toString(result)+"C";
+        }
+        else{
+            out.setText("请输入单位C/c或F/f");
+            return null;
+        }
+        return res;
 
     }
+
 }
