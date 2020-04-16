c5c8fa179a249819e602f2b5e7922b0e56cd97bb
Wyh987654321 <57186610+Wyh987654321@users.noreply.github.com>
2020-04-16 16:58:23 +0800

    积分app添加横屏页面

--- app/src/main/java/com/swufe/firstapp/SecondActivity.java
+++ app/src/main/java/com/swufe/firstapp/SecondActivity.java
@@ -1,6 +1,7 @@
 package com.swufe.firstapp;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 
@@ -15,7 +16,32 @@ public class SecondActivity extends AppCompatActivity {
         setContentView(R.layout.activity_second);
         score =findViewById(R.id.score);
         scoreb =findViewById(R.id.scoreb);
+        Log.i("second","没出问题了");
     }
+
+    @Override
+    public void onSaveInstanceState( Bundle outState ) {
+        super.onSaveInstanceState(outState);
+        score =findViewById(R.id.score);
+        scoreb =findViewById(R.id.scoreb);
+        String scorea=score.getText().toString();
+        String scoreb2=scoreb.getText().toString();
+        outState.putString("teama_score",scorea);
+        outState.putString("teamb_score",scoreb2);
+
+    }
+
+    @Override
+    protected void onRestoreInstanceState( Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        score =findViewById(R.id.score);
+        scoreb =findViewById(R.id.scoreb);
+        String scorea = savedInstanceState.getString("teama_score");
+        String scoreb2 = savedInstanceState.getString("teamb_score");
+        score.setText(scorea);
+        scoreb.setText(scoreb2);
+    }
+
     public void add(View btn) {
         if (btn.getId() == R.id.btn3b) {
             String olds = scoreb.getText().toString();
@@ -23,6 +49,10 @@ public class SecondActivity extends AppCompatActivity {
                 int trues = Integer.parseInt(olds);
                 trues+=3;
                 scoreb.setText("" + trues);
+            }
+            else{
+                scoreb.setText("0");
+                add(btn);
             }
         }
         if (btn.getId() == R.id.btn2b) {
@@ -32,6 +62,10 @@ public class SecondActivity extends AppCompatActivity {
                 trues+=2;
                 scoreb.setText("" + trues);
             }
+            else{
+                scoreb.setText("0");
+                add(btn);
+            }
         }
         if (btn.getId() == R.id.btn1b) {
             String olds = scoreb.getText().toString();
@@ -40,12 +74,20 @@ public class SecondActivity extends AppCompatActivity {
                 trues+=1;
                 scoreb.setText("" + trues);
             }
+            else{
+                scoreb.setText("0");
+                add(btn);
+            }
         }if (btn.getId() == R.id.btn3) {
             String olds = score.getText().toString();
             if (olds.length() > 0) {
                 int trues = Integer.parseInt(olds);
                 trues+=3;
                 score.setText("" + trues);
+            }
+            else{
+                score.setText("0");
+                add(btn);
             }
         }
         if (btn.getId() == R.id.btn2) {
@@ -55,6 +97,10 @@ public class SecondActivity extends AppCompatActivity {
                 trues+=2;
                 score.setText("" + trues);
             }
+            else{
+                score.setText("0");
+                add(btn);
+            }
         }
         if (btn.getId() == R.id.btn1) {
             String olds = score.getText().toString();
@@ -62,6 +108,10 @@ public class SecondActivity extends AppCompatActivity {
                 int trues = Integer.parseInt(olds);
                 trues+=1;
                 score.setText("" + trues);
+            }
+            else{
+                score.setText("0");
+                add(btn);
             }
         }
     }
