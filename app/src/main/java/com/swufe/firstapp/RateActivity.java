0ca96df6b88d8d582db1e23b1f352d16bd8821c7
Wyh987654321 <57186610+Wyh987654321@users.noreply.github.com>
2020-04-28 20:11:28 +0800

    添加每日只获取一次数据功能

--- app/src/main/java/com/swufe/firstapp/RateActivity.java
+++ app/src/main/java/com/swufe/firstapp/RateActivity.java
@@ -28,6 +28,7 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.Reader;
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
 
 public class RateActivity extends AppCompatActivity implements Runnable {
@@ -39,11 +40,7 @@ public class RateActivity extends AppCompatActivity implements Runnable {
     float dollar_rate=0.13f;
     float euro_rate=0.14f;
     float won_rate=173.64f;
-    private  int date;  //用于存储日期，当日期发生改变时，值也会改变
-    SharedPreferences sp2;    //用于从SharedPreferences中取出date日期
-    SharedPreferences share;  //用于存储date
-    SharedPreferences.Editor editor;//share的editor对象
-    int truedate;
+    private  String updateDate="";
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_rate);
@@ -54,43 +51,45 @@ public class RateActivity extends AppCompatActivity implements Runnable {
         dollar_rate = sp.getFloat("dollar_rate", 0.0f);
         euro_rate = sp.getFloat("euro_rate", 0.0f);
         won_rate = sp.getFloat("won_rate", 0.0f);
-        sp2 = getSharedPreferences("mydate", Activity.MODE_PRIVATE);//实例化
-        share= super.getSharedPreferences("mydate", MODE_PRIVATE);//实例化
-        editor= share.edit();    //使处于可编辑状态
-        //开启子线程
-        Thread t = new Thread(this);
-        t.start();
+        updateDate=sp.getString("update_date","");
+        //获取当前系统时间
+        final Date today=Calendar.getInstance().getTime();
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+        final String todayStr = sdf.format(today);
+        Log.i("run","todayStr="+todayStr);
+        Log.i("run","updateDate="+updateDate);
+        //判断时间
+        if(!todayStr.equals(updateDate)){
+            Log.i("run","需要更新");
+            //开启子线程
+            Thread t = new Thread(this);
+            t.start();
+
+        }
+        else{
+            Log.i("run","不需要更新");
+        }
+
 
         handler = new Handler() {
             @Override
             public void handleMessage(@NonNull Message msg) {
-                date=sp2.getInt("date",0);   //取出SharedPreferences中的日期
                 if (msg.what == 5) {
                     Bundle bundle = (Bundle) msg.obj;
-                    /*
-                    两个日期进行比较也就是date和truedate，如果相同，则代表是同一天
-                    那么数据不用更新，直接return;
-                    如果不同，则代表不是同一天，那么更新数据，然后让date=truedate,
-                    至此完成一天的更新，直到第二天打开app获取truedate发现日期不匹配，
-                    再进行数据更新
-                     */
-                    if(date==bundle.getInt("date")){
-                        return;
-                    }
-                    else{
-                        date=bundle.getInt("date");
-                        Log.i("run","不相等"+date);
-                        dollar_rate = bundle.getFloat("dollar-rate");
-                        euro_rate = bundle.getFloat("euro-rate");
-                        won_rate = bundle.getFloat("won-rate");
-                        Log.i("run","数据更新了");
+                    dollar_rate = bundle.getFloat("dollar-rate");
+                    euro_rate = bundle.getFloat("euro-rate");
+                    won_rate = bundle.getFloat("won-rate");
+                    Toast.makeText(RateActivity.this, "汇率已更新", Toast.LENGTH_SHORT).show();
 
-                        editor.putInt("date", date);
-                        editor.commit();
-                        Toast.makeText(RateActivity.this, "汇率已更新", Toast.LENGTH_SHORT).show();
+                    //保存更新的日期
+                    SharedPreferences sp = getSharedPreferences("myrate", Activity.MODE_PRIVATE);
+                    SharedPreferences.Editor editor = sp.edit();
+                    editor.putString("update_date",todayStr);
+                    editor.putFloat("dollar_rate",dollar_rate);
+                    editor.putFloat("euro_rate",euro_rate);
+                    editor.putFloat("won_rate",won_rate);
+                    editor.commit();
                     }
-
-                }
                 super.handleMessage(msg);
             }
         };
@@ -162,48 +161,22 @@ public class RateActivity extends AppCompatActivity implements Runnable {
     }
     public void run(){
 
-
-        //获取网络数据
-//        try {
-//              URL url = new URL("http://www.usd-cny.com/");
-//
-//            HttpURLConnection http =(HttpURLConnection)url.openConnection();
-//            InputStream inp = http.getInputStream();
-//
-//            String html=inputStream2String(inp);
-//            Log.i("run","htmls "+html);
-//            Document doc= Jsoup.parse(html);
-//        } catch (MalformedURLException e) {
-//            e.printStackTrace();
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//            Log.i("run",e+"");
-//        }
         //用于保存获取的汇率
-        Bundle bundle = new Bundle();
+        Bundle bundle = get_webdata();
+        //获取msg对象，用于返回主线程
+        Message msg =handler.obtainMessage(5);
+        msg.obj=bundle;
+        handler.sendMessage(msg);
+    }
+
+    private Bundle get_webdata() {
+        Bundle bundle =new Bundle();
         Document doc = null;
-        /*
-            获取时间，每次执行run方法都会获取时间，用于之后比较
-         */
-        SimpleDateFormat formatter= new SimpleDateFormat("dd 'at' HH:mm:ss z");
-        Date date2 = new Date(System.currentTimeMillis());
-        String time =date2.toString();
-        truedate =Integer.parseInt(time.substring(8,10).trim());  //从时间中取出日期
-        Log.i("run","truedate="+truedate);
-        date=sp2.getInt("date",0); //取出SharedPreferences中的date
-        /*
-        理论上这个if条件只会执行一次，就是代码修改后的第一次editor还没有还没有向SP中放数据的时候
-         */
-        if(date==0) {     //向SP中存放date数据，其值为0，意味着第一次一定会更新数据
-            editor.putInt("date", truedate);
-            editor.commit();
-        }
-
         try {
-            doc = Jsoup.connect("http://www.usd-cny.com/bankofchina.htm").get();
 
+            doc = Jsoup.connect("http://www.usd-cny.com/bankofchina.htm").get();
             Elements tables = doc.getElementsByTag("table");
-            Element  table1 = tables.get(0);
+            Element table1 = tables.get(0);
             //获取TD中的元素
             Elements tds =table1.getElementsByTag("td");
             for(int i=0;i<tds.size();i+=6){
@@ -219,15 +192,11 @@ public class RateActivity extends AppCompatActivity implements Runnable {
                     bundle.putFloat("won-rate",100f/Float.parseFloat(td2.text()));
                 }
             }
-            //获取msg对象，用于返回主线程
-            bundle.putInt("date",truedate);   //将日期放在bundle中
-            Message msg =handler.obtainMessage(5);
-            msg.obj=bundle;
-            handler.sendMessage(msg);
+
         } catch (IOException e) {
             e.printStackTrace();
         }
-
+        return bundle;
     }
 
     public static String inputStream2String (InputStream inputStream) throws IOException   {
